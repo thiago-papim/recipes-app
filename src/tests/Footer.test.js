0169796a9592e5dcd9a28ea056026b3422126d08
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-04-03 22:13:34 -0300

    Reqs 16-18

---
+++ src/tests/Footer.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import Footer from '../components/Footer';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testando o Footer', () => {
+  it('Testando se existem os 2 ícones na tela', () => {
+    renderWithRouter(<Footer />);
+    const drinkIcon = screen.getByTestId(/drinks-bottom-btn/i);
+    const mealIcon = screen.getByTestId(/meals-bottom-btn/i);
+    const footerId = screen.getByTestId(/footer/i);
+    expect(drinkIcon).toBeInTheDocument();
+    expect(mealIcon).toBeInTheDocument();
+    expect(footerId).toBeInTheDocument();
+  });
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/drinks');
+    });
+    expect(history.location.pathname).toBe('/drinks');
+  });
+});
