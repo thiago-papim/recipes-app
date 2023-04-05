e29418c1412442fae3eb07c7c139bfa60761020d
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-04-05 15:22:57 -0300

    Req 25 iniciando

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import AppContext from '../context/AppContext';
 
 function RecipeDetails(props) {
   const [details, setDetails] = useState([]);
